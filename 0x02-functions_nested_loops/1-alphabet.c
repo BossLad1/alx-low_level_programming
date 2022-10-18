758f094b2b6415171c5ca6bbd09953cf362f5c10
Hauwa Ibrahim <hauwaibrahim71@gmail.com>
2022-10-18 14:47:58 -0700

    T1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main- will return lowercase alphabet
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	for (y = 'a'; y <= 'z'; y++)
+	{
+		_putchar('y');
+	}
+	_putchar('\n');
+	return (0);
+}
