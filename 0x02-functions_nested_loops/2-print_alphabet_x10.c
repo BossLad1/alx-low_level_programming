88caabe67b2bc01257bd09adafaf559dbec42039
Hauwa Ibrahim <hauwaibrahim71@gmail.com>
2022-10-18 15:28:05 -0700

    T2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
  * print_alphabet_x10 - print lowercase alphabet x10
- * Return: Always 0
  */
 
 void print_alphabet_x10(void);
@@ -9,29 +8,11 @@ void print_alphabet_x10(void);
 	int y;
 	int z;
 
-	for (y = 'a'; y <= 10; y++)
+	for (z = 'a'; z <= 10; z++)
 	{
-		for (z = 'a'; z <= 10; z++)
+		for (y = 'a'; y <= 10; y++)
 		{
 			_putchar(y);
 		}
 		_putchar('\n');
 }
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
--- INSERT --                                                  6,1           All
-
