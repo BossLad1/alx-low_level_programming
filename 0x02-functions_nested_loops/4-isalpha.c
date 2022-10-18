0b074c69759c7153caee4faab3f4536fdb52e63f
Hauwa Ibrahim <hauwaibrahim71@gmail.com>
2022-10-18 15:44:13 -0700

    T2

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isalpha - checks if character is upper case
+ * @c: a character argument
+ * Return: 1 or 0 depending on condition
+ */
+int _isalpha(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
