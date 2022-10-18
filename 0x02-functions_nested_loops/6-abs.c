9eea9f897fb11a62b611a495b35200037746a963
Hauwa Ibrahim <hauwaibrahim71@gmail.com>
2022-10-18 16:43:33 -0700

    Task 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-* _abs - computes the absolute value of an integer
+ * _abs - computes the absolute value of an integer
  * @i: parameter
  * Return: absolute value of a number
  */
