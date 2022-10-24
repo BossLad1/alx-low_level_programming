ee5fb9dec71004031dd78a361834a210885794ad
Hauwa Ibrahim <hauwaibrahim71@gmail.com>
2022-10-24 14:37:18 -0700

    Task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
  * swap_int - Swaps the value of two integers
- * @*a: first parameter
- * @*b: second parameter
+ * @a: first parameter
+ * @b: second parameter
  */
 void swap_int(int *a, int *b)
 {
-	tmp = *a
-	*a  = *b
-	*b  = tmp
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
 }
