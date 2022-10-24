2aed8ae451250eb44df5318e7273e2094b96ff07
Hauwa Ibrahim <hauwaibrahim71@gmail.com>
2022-10-24 15:39:53 -0700

    Task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,9 +5,12 @@
  */
 void print_rev(char *s)
 {
-	while (*s != '\0')
-	{
-		_putchar(*s--);
-	}
+	int m;
+
+	while (s[m] != '\0')
+		m++
+	for (m = m - 1; m >= 0; m--)
+		_putchar(s[m]);
+
 	_putchar('\n');
 }
