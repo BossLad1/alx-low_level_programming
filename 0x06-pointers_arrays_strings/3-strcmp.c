4324af06a964f82db40114fd87b1b83728415f49
Hauwa Ibrahim <hauwaibrahim71@gmail.com>
2022-10-27 04:45:49 -0700

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,13 +3,13 @@
  * _strcmp - a function that compares two strings
  * @s1: input to compare
  * @s2: against this other string 2
- * Return: 0 if s1 and s2 are equal
+ * Return: 0 if s1 and s2 are equals, another number if not
  */
 int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++);
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 	{
 		if (s1[i] != s2[i])
 		{
