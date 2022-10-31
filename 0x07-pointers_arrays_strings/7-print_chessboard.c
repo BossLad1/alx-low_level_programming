4311d3450f1e740499d3df1e33e11a8c8e8ee4c9
Hauwa Ibrahim <hauwaibrahim71@gmail.com>
2022-10-31 13:02:17 -0700

    task

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_chessboard - print the chessboard, 2d array
+ * @a: 2d array of chars
+ * Return: void
+ */
+void print_chessboard(char (*a)[8])
+{
+	int c, b;
+
+	c = 0;
+	while (c < 8)
+	{
+		b = 0;
+		while (b < 8)
+		{
+			_putchar (a[c][b]);
+			b++;
+		}
+		_putchar ('\n');
+		c++;
+	}
+}
