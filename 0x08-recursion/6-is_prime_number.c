996de8a90bde98f55b2e88186c6bb8ba6710471d
Hauwa Ibrahim <hauwaibrahim71@gmail.com>
2022-11-02 13:05:22 -0700

    Task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * is_prime - detects if an input number is a prime number.
- * @n: input number
+ * @n: input number.
  * @c: iterator.
  * Return: 1 if n is a prime number. 0 if n is not a prime number.
  */
@@ -18,7 +18,7 @@ int is_prime(unsigned int n, unsigned int c)
 }
 /**
  * is_prime_number - detects if an input number is a prime number.
- * @n: input number
+ * @n: input number.
  * Return: 1 if n is a prime number. 0 if n is not a prime number.
  */
 int is_prime_number(int n)
