21b27323e1c37ba6412eef2fe56a84e9e3ab6db0
BossLad1 <hauwaibrahim71@gmail.com>
2022-11-04 13:19:14 -0700

    task 1

--- 0x0A-argc_argv/1-argc.c
+++ 0x0A-argc_argv/1-argc.c
@@ -5,7 +5,7 @@
  * @argv: array that contains the program command line arguments.
  * Return: 0 - success.
  */
-int main(int argc, char *argv[] __attribute__((unused)))
+int main(int argc, __attribute__((unused)) char *argv[])
 {
 	printf("%d\n", argc - 1);
 	return (0);
