ffe08f764e2f7836193f8529533975d144340e28
BossLad1 <hauwaibrahim71@gmail.com>
2022-11-07 14:01:56 -0800

    task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,28 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
 /**
- * main - adds positiv numbers.
- * @argc: number of command line arguments.
- * @argv: array that contains the program command line arguments.
+ * check_num - check - string there are gigit
+ * @str: array str
  * Return: 0 - success.
+ */
+int check_num(char *str)
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < strlen(str))
+	{
+		if (!isdigit(str[i]))
+		{
+			return (0);
+		}
+		i++;
+	}
+	return (1);
+}
+
+/**
+ * main - print the name of the program
+ * @argc: Count arguments
+ * @argv: Arguments
+ * Return: Always 0
  */
 int main(int argc, char *argv[])
 {
-	int i, j, add = 0;
+	int i;
+	int str_to_int;
+	int sum = 0;
 
-	for (i = 1; i < argc; i++)
+	i = 1;
+	while (i < argc)
 	{
-		for (j = 0; argv[i][j] != '\0'; j++)
+		if (check_num(argv[i]))
 		{
-			if (!isdigit(argv[i][j]))
-			{
-				printf("Error\n");
-				return (1);
-			}
+			str_to_int = atoi(argv[i]);
+			sum += str_to_int;
 		}
-		add += atoi(argv[1]);
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+		i++;
 	}
-	printf("%d\n", add);
+	printf("%d\n", sum);
 	return (0);
 }
