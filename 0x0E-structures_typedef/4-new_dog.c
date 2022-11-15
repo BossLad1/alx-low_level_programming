6c88af5f934c25026b7a99f9c44729cf64c7a786
BossLad1 <hauwaibrahim71@gmail.com>
2022-11-15 09:52:13 -0800

    Task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -22,7 +22,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		cutie_dog = malloc(sizeof(dog_t));
 		if (cutie_dog == NULL)
 			return (NULL);
-		cutie_dog->name = malloc(sizeof(char) * name_l)
+		cutie_dog->name = malloc(sizeof(char) * name_l);
 			if (cutie_dog->name == NULL)
 			{
 				free(cutie_dog);
@@ -73,5 +73,3 @@ char *_strcpy(char *dest, char *src)
 	dest[i++] = '\0';
 	return (dest);
 }
-}
-
